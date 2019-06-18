ded616d393bbccb1aa899f53411810d60e9806fb
yawnlon <wuboyangyawn@hotmail.com>
2019-06-18 23:41:54 +0800

    多线程自动化部署

--- backend/utils/webhook.py
+++ backend/utils/webhook.py
@@ -1,8 +1,12 @@
 from xueqing.settings import BASE_DIR
 import os
+from threading import Thread
 
 SECRET = "YzfCGkrIIWZUtnkU4RiFkBdO4Eyjrrm9"
 RELOAD_CMD = "bash reload.sh"
+
+def reload():
+    Thread(target=do_reload).start()
 
 def do_reload():
     os.chdir(BASE_DIR)
