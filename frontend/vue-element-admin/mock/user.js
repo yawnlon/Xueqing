b600e9aa22f2f2dd573175b51cd3132babf80841
whichb <dfnanhai@sina.com>
2019-06-24 16:17:43 +0800

    update

--- frontend/vue-element-admin/mock/user.js
+++ frontend/vue-element-admin/mock/user.js
@@ -81,23 +81,23 @@ export default [
     }
   },
 
-  {
-    url: '/account/register',
-    type: 'post',
-    response: _ => {
-      return {
-        "detail":"注册成功",
-        "data":{
-            "user":{
-                "id":10,// user id
-                "username":"xq_18813042473",
-                "mobile":"18813042473",
-                "type":"student",// 用户类型(分为4种，(0, 'student'), 学生用户// (1, 'admin'), 管理员// (2, 'school'), 学校用户// (3, 'agent')) 代理商
-                "is_active":true// 是否活跃，false为已冻结
-            },
-            "token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMCwidXNlcm5hbWUiOiJ4cV8xODgxMzA0MjQ3MyIsImV4cCI6MTU2MDY5MDAzMywiZW1haWwiOiIifQ.c_pXZ_XvSuvqy65Xb1WtdXc_ropZcpldpYiQWSXV5NM"
-        }
-      }
-    }
-  }
+  // {
+  //   url: '/account/register',
+  //   type: 'post',
+  //   response: _ => {
+  //     return {
+  //       "detail":"注册成功",
+  //       "data":{
+  //           "user":{
+  //               "id":10,// user id
+  //               "username":"xq_18813042473",
+  //               "mobile":"18813042473",
+  //               "type":"student",// 用户类型(分为4种，(0, 'student'), 学生用户// (1, 'admin'), 管理员// (2, 'school'), 学校用户// (3, 'agent')) 代理商
+  //               "is_active":true// 是否活跃，false为已冻结
+  //           },
+  //           "token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMCwidXNlcm5hbWUiOiJ4cV8xODgxMzA0MjQ3MyIsImV4cCI6MTU2MDY5MDAzMywiZW1haWwiOiIifQ.c_pXZ_XvSuvqy65Xb1WtdXc_ropZcpldpYiQWSXV5NM"
+  //       }
+  //     }
+  //   }
+  // }
 ]
